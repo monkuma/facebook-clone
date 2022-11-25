778925ea9fb42823a8744c55f9cb60226b076ff2
monkuma <kumamoto.jobs@gmail.com>
2022-11-25 18:09:49 +0900

    first commit

---
+++ components/SideBar.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import React from "react";
+import SideBarItems from "./SideBarItems";
+import { ImUsers } from "react-icons/im";
+import {
+  MdGroups,
+  MdOutlineOndemandVideo,
+  MdOutlineExpandMore,
+} from "react-icons/md";
+import { AiOutlineShop } from "react-icons/ai";
+import { BsStopwatch } from "react-icons/bs";
+
+const SideBar = () => {
+  return (
+    <div
+      className="hidden lg:inline-flex flex-col py-2 pl-2
+    max-w-xl lf:min-w-[320px]"
+    >
+      <div
+        className="flex items-center space-x-2 py-3 pl-4 hover:bg-gray-200 rounded-l-xl
+    cursor-pointer"
+      >
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/800px-2021_Facebook_icon.svg.png"
+          height={40}
+          width={40}
+          className="rounded-full cursor-pointer"
+        />
+        <p className="hidden sm:inline-flex font-medium">Shabby Dawoodi</p>
+      </div>
+      <SideBarItems Icon={ImUsers} value="Friends" />
+      <SideBarItems Icon={MdGroups} value="Groups" />
+      <SideBarItems Icon={AiOutlineShop} value="Marketplace" />
+      <SideBarItems Icon={MdOutlineOndemandVideo} value="Watch" />
+      <SideBarItems Icon={BsStopwatch} value="Memories" />
+      <SideBarItems Icon={MdOutlineExpandMore} value="See More" />
+    </div>
+  );
+};
+
+export default SideBar;
